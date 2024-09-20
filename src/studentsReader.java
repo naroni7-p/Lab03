d4189725380ef332804a880850eac1a9a6c7b4a5
leen <leen762005@gmail.com>
2024-09-20 14:44:05 +0300

    I added a message that will be printed if the list is empty

--- src/studentsReader.java
+++ src/studentsReader.java
@@ -3,6 +3,10 @@ import java.util.List;
 public class studentsReader {
     public static void readStudents(List<String> studentsNames)
     {
+        if (studentsNames == null || studentsNames.isEmpty()) {
+            System.out.println("No students to display.");
+            return;
+        }
         for(String student:studentsNames)
         System.out.println(student);
     }
